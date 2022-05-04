b2bff2e40225e40c2f937e085843854f7a95eb6e
rinjyu <rinjyu@naver.com>
2022-05-04 21:54:10 +0900

    feat: 3.2.1 Step에 대한 이해

---
+++ src/main/java/io/springbatch/study/configuration/StepConfiguration.java
@@ -0,0 +1,44 @@
+package io.springbatch.study.configuration;
+
+import io.springbatch.study.CustomTasklet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class StepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job BatchJob() {
+        return this.jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new CustomTasklet())
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> step2 <<");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
