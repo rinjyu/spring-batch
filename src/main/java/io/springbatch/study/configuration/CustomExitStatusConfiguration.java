69cf03afde3788747b08b9f019d1325a5e678d13
rinjyu <rinjyu@naver.com>
2022-05-29 12:11:15 +0900

    feat: 6.5 사용자 정의 ExitStatus

---
+++ src/main/java/io/springbatch/study/configuration/CustomExitStatusConfiguration.java
@@ -0,0 +1,54 @@
+package io.springbatch.study.configuration;
+
+import io.springbatch.study.PassCheckingListener;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class CustomExitStatusConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return this.jobBuilderFactory.get("batchJob")
+                .start(step1())
+                    .on("FAILED")
+                    .to(step2())
+                    .on("PASS")
+                    .stop()
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> step1 <<");
+                    contribution.setExitStatus(ExitStatus.FAILED);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+    
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> step2 <<");
+                    return RepeatStatus.FINISHED;
+                })
+                .listener(new PassCheckingListener())
+                .build();
+    }
+}
