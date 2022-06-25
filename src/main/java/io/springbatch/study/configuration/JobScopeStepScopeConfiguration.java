75d5aed17e72d354d2b8d25a3759df6db2eb3249
rinjyu <rinjyu@naver.com>
2022-06-25 12:03:38 +0900

    feat: 6.13 @JobScope / @StepScope 아키텍처

---
+++ src/main/java/io/springbatch/study/configuration/JobScopeStepScopeConfiguration.java
@@ -0,0 +1,44 @@
+package io.springbatch.study.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobScopeStepScopeConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                .start(step(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step step(@Value("#{jobParameters['message']}") String message) {
+        System.out.println("jobParameters['message'] : " + message);
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}
