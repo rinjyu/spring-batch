d2ea469be425e6223836f2d0f7391bffb522f36d
rinjyu <rinjyu@naver.com>
2022-07-16 08:25:17 +0900

    feat: 7.4 ItemReader / ItemWriter / ItemProcessor의 이해

---
+++ src/main/java/io/springbatch/study/Customer.java
@@ -0,0 +1,10 @@
+package io.springbatch.study;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+    private String name;
+}
---
+++ src/main/java/io/springbatch/study/configuration/ItemReaderProcessorWriterConfiguration.java
@@ -0,0 +1,72 @@
+package io.springbatch.study.configuration;
+
+import io.springbatch.study.CustomItemProcessor;
+import io.springbatch.study.CustomItemReader;
+import io.springbatch.study.CustomItemWriter;
+import io.springbatch.study.Customer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@RequiredArgsConstructor
+public class ItemReaderProcessorWriterConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Customer, Customer>chunk(3)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    public ItemReader<Customer> itemReader() {
+        return new CustomItemReader(Arrays.asList(new Customer("user1"),
+                new Customer("user2"),
+                new Customer("user3")));
+    }
+
+    @Bean
+    public ItemProcessor<? super Customer, ? extends Customer> itemProcessor() {
+        return new CustomItemProcessor();
+    }
+
+    @Bean
+    public ItemWriter<? super Customer> itemWriter() {
+        return new CustomItemWriter();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> step2 <<");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
