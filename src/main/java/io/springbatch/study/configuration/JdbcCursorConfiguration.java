86dc1cd0e422fc73a5b31ec4c3a082de7b157391
rinjyu <rinjyu@naver.com>
2022-08-08 20:27:16 +0900

    feat: 8.9 DB - JdbcCursorItemReader에 대한 이해

---
+++ src/main/java/io/springbatch/study/configuration/JdbcCursorConfiguration.java
@@ -0,0 +1,67 @@
+package io.springbatch.study.configuration;
+
+import io.springbatch.study.Customer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcCursorConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+    private int chunkSize = 10;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Customer, Customer>chunk(chunkSize)
+                .reader(customItemReader())
+                .writer(customItemWriter())
+                .build();
+    }
+
+    @Bean
+    public ItemReader<Customer> customItemReader() {
+        return new JdbcCursorItemReaderBuilder()
+                .name("jdbcCursorItemReader")
+                .fetchSize(chunkSize)
+                .sql("select id, firstName, lastName, birthdate from customer where firstName like ? order by lastName, firstName")
+                .beanRowMapper(Customer.class)
+                .queryArguments("A%")
+//                .maxItemCount(3)
+//                .currentItemCount(2)
+//                .maxRows(100)
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<Customer> customItemWriter() {
+        return items -> {
+            for (Customer item : items) {
+                System.out.println(item.toString());
+            }
+        };
+    }
+}
